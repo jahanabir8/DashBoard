ed52df38919c38b05fc36fe3bedc64a064cafc20
jahanabir8 <abir.opediatech@gmail.com>
2022-09-15 13:04:55 +0600

    dashboard

---
+++ script.js
@@ -0,0 +1,25 @@
+
+// function toggleMenu(){
+//     let toggle = document.querySelector('.toggle');
+//     let navigation = document.querySelector('.navigation');
+//     let main = document.querySelector('.main');
+//     toggle.classList.toggle('active');
+//     navigation.classList.toggle('active');
+// }
+
+// toggle.onclick = function(){
+//     navigation.classList.toggle('active');
+//     main.classList.toggle('active');
+// }
+
+
+
+// 
+let list = documnet.querySelectorAll('.navigation li');
+function activeLink(){
+    list.forEach((item) =>
+        item.classList.remove('hovered'));
+        this.classList.add('hovered');
+}
+list.forEach((item) =>
+item.addEventListener('mouseover', activeLink));
